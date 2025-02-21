75dac45dda3a67777e45459555b5c95febf9f883
GrishMahat <grishmahat@gmail.com>
2025-02-21 06:28:45 +0545

    Update copyright years and add license headers
    - Update copyright year to 2025 in LICENSE and source files
    - Add full Apache License headers to converter.rs and viewer.rs
    - Add documentation comment in main.rs

--- src/format.rs
+++ src/format.rs
@@ -1,4 +1,4 @@
-// Copyright 2023 Grish
+// Copyright 2025 Grish
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-// Copyright 2023 Grish
+// Copyright 2023` Grish
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+//! Most of the documentation is by AI, I am to lazy to write it myself.
 //! # Nor-Image CLI
 //! 
 //! Command-line interface for the Nor-Image tool, providing functionality for:
